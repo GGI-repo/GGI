8ef6bcc39f02997fa1d4ce7f887d09181ecc6e5f
Driss Benmoumen <benmoumendriss.94@gmail.com>
2017-05-06 20:54:59 +0200

    Unit test configuration dbunit

--- src/test/java/company/ggi/dao/CategoryDaoTest.java
+++ src/test/java/company/ggi/dao/CategoryDaoTest.java
@@ -1,5 +1,10 @@
 package company.ggi.dao;
 
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.DatabaseTearDown;
+import com.github.springtestdbunit.annotation.DbUnitConfiguration;
+import com.github.springtestdbunit.dataset.FlatXmlDataSetLoader;
 import company.ggi.model.Category;
 import org.junit.Assert;
 import org.junit.Before;
@@ -7,8 +12,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 import org.springframework.test.context.web.AnnotationConfigWebContextLoader;
 import org.springframework.test.context.web.WebAppConfiguration;
 
@@ -17,10 +27,14 @@ import org.springframework.test.context.web.WebAppConfiguration;
  */
 
 @RunWith(SpringRunner.class)
-@WebAppConfiguration
+@DbUnitConfiguration(dataSetLoader = FlatXmlDataSetLoader.class)
 @ContextConfiguration(loader = AnnotationConfigWebContextLoader.class, classes = {company.ggi.config.WebAppConfig.class})
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@SuppressWarnings("SpringJavaAutowiringInspection")
+@TestExecutionListeners({DependencyInjectionTestExecutionListener.class,
+        DirtiesContextTestExecutionListener.class,
+        TransactionalTestExecutionListener.class,
+        DbUnitTestExecutionListener.class})
+@WebAppConfiguration
+@DatabaseSetup("/META-INF/dataSets.xml")
 public class CategoryDaoTest {
 
     @Autowired
